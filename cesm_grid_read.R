2c6f861891ff74467bea0b687a20af540436aa14
William J. Capehart <William.Capehart@sdsmt.edu>
2017-06-08 22:23:26 -0600

    Several Updates to R file including more commenting

--- cesm_grid_read.R
+++ cesm_grid_read.R
@@ -2,14 +2,19 @@
 
 library(ncdf4)
 library(maps)
+library(mapdata)
 
 
-# get specific areas to extract..
+# Let's start by identifying specific points, days and ensembles from which to pull data.
+#   it's nice to put this data up top to make changing locatons easier.
 
 target_lon  <- -104.
 target_lat  <-   44.
 target_ens  <-    1
-target_date <- as.Date("2050-01-01")
+target_date <- as.Date("2050-07-01") # you need to wrap the date in the 
+                                     # as.Date() function
+
+
 
 
 # get URL root location (comment out if in windows)
@@ -21,9 +26,22 @@ URL_Root_Directory <- "http://kyrill.ias.sdsmt.edu:8080/thredds/dodsC/testAll/"
 
 # files can be found at :"http://kyrill.ias.sdsmt.edu:8080/thredds/catalog.html"
 
+
+# Here is the variable we want to extract
+#  these should match the files names since the variable in the file and file name match
+target_variable = "TSA"
+variable_name   = "Monthly Mean 2-m Air Temperature"
+variable_units  = "deg F"
+
+
 # get URL files
-RCP_85_File <- "TSA_RCP85_CONUS_1920-2100.nc4"
-RCP_45_File <- "TSA_RCP45_CONUS_1920-2080.nc4"
+RCP_85_File <- paste(target_variable, 
+                     "_RCP85_CONUS_1920-2100.nc4", 
+                     sep="")
+
+RCP_45_File <- paste(target_variable, 
+                     "_RCP45_CONUS_1920-2080.nc4", 
+                     sep="")
 
 # make the final URLs for extracting with the "paste" function
 
@@ -40,8 +58,8 @@ URL_45 <- paste(URL_Root_Directory,  # string 1 to concatenate
 # we now open the files...
 #   the output of these functions are "handles" by which we can reference the file
 
-nc.85 <- nc_open(filename=URL_85)
-nc.45 <- nc_open(filename=URL_45)
+nc.85 <- nc_open(filename = URL_85)
+nc.45 <- nc_open(filename = URL_45)
 
 # to view the inventory (or "metadata") of a netCDF file you can "print()" the metadata
 #   let's do this with the RCP 8.5 file
@@ -55,7 +73,7 @@ print(nc.85)
 
 time_months.85 <- seq(as.Date("1920-01-01"),  # start value
                       as.Date("2080-12-01"),  # end value
-                      "months"                # increment
+                      "months"                # increment 
                      )
 
 
@@ -74,7 +92,7 @@ time_months.45 <- seq(as.Date("1920-01-01"),  # start value
 time_bounds.85 <- ncvar_get(nc    = nc.85,          # netcdf file ID
                             varid = "time_bounds",  # variable name from file
                             start = c(1,   1),      # starting coordinates
-                            count = c(2,1932)       # end coordinates
+                            count = c(2,1932)       # end coordinates (pulling data only to 2080)
                            )
 
 time_bounds.45 <- ncvar_get(nc    = nc.45,         # netcdf file ID
@@ -85,7 +103,6 @@ time_bounds.45 <- ncvar_get(nc    = nc.45,         # netcdf file ID
 
 days_per_month.85 <- time_bounds.85[2, ] - time_bounds.85[1, ]
 days_per_month.45 <- time_bounds.45[2, ] - time_bounds.45[1, ]
-
 
 # import spatial coordinates
 
@@ -99,123 +116,192 @@ lat <- ncvar_get(nc    = nc.85,         # netcdf file ID
 
 # location information
 
-lon <- lon - 360.0
+lon <- lon - 360.0  # converting to +/- deg east for easier plotting
+
+# Getting Map extents
 
 minlon <- min(lon) 
 maxlon <- max(lon) 
 minlat <- min(lat)
 maxlat <- max(lat)
 
+#  with time, latitude and longitude we can now extract the specific points
+#   in our files to extract for x, y and t
+ 
+target_x <- which.min( abs(            lon -  target_lon) ) 
+target_y <- which.min( abs(            lat -  target_lat) ) 
+target_t <- which.min( abs( time_months.85 - target_date) ) 
+ 
+# import data fields (first RCP 4.5)
 
-target_x <- which.min(abs(           lon - target_lon)) 
-target_y <- which.min(abs(           lat - target_lat)) 
-target_t <- which.min(abs(time_months.85 - target_date)) 
-
-# import data coordinates
-
-t_2m.45 <- ncvar_get(nc      = nc.45,  # netcdf file ID
-                     varid   = "TSA",  # variable name from file
-                     verbose = TRUE
+var.45 <- ncvar_get(nc      = nc.45,            # netcdf file ID
+                    varid   = target_variable,  # variable name from file
+                    verbose =  FALSE            # print diagnostic data
                     )
 
+# import data fields (secondly RCP 4.5)
 
-t_2m.85 <- ncvar_get(nc      = nc.85,  # netcdf file ID
-                     varid   = "TSA",  # variable name from file
-                     verbose =  TRUE,
-                     start = c( 1, 1, 1,   1), # starting coordinates
-                     count = c(62,34,30,1932) # end coordinates
+
+var.85 <- ncvar_get(nc      = nc.85,              # netcdf file ID
+                    varid   = target_variable,    # variable name from file
+                    verbose = TRUE,               # print diagnostic data
+                    start   = c( 1, 1, 1,   1),   # starting coordinates
+                    count   = c(62,34,30,1932)    # end coordinates (we're only going to 2080)
                     )
 
+# converting units for civilian friendly values (e.g., for temperature)
 
-# making a map
+if ( (target_variable == "TSA") && (variable_units == "deg C") ) 
+  {
+     var.45 = var.45 - 273.17     # K -> deg C
+     var.85 = var.85 - 273.17     # K -> deg C
+  }
+
+if ( (target_variable == "TSA") && (variable_units == "deg F") ) 
+  {
+    var.45 = 1.8 * var.45 - 459.67  # K -> deg F
+    var.85 = 1.8 * var.85 - 459.67  # K -> deg F
+  }
+
+# making a map  There are a lot of ways to do this.
+
+# this method uses a set of axes with mapping data attachd
+
+# extract specific ensemble and time
+plot_var <- var.45[,, target_ens, target_t]
+
+max_plotting_range = max(c(var.45[,, target_ens, target_t],
+                           var.85[,, target_ens, target_t]),
+                         na.rm=TRUE)  # na.rm=True : don't count missing values
+
+min_plotting_range = min(c(var.45[,, target_ens, target_t],
+                           var.85[,, target_ens, target_t]),
+                         na.rm=TRUE)  # na.rm=True : don't count missing values
 
 
-plot_var =  t_2m.45[,,target_ens, target_t] - 273.17
-
-filled.contour(x = lon, 
-               y = lat,
-               z = plot_var, 
-               color.palette = colorRampPalette(c("violet",
+# create contour map
+filled.contour(x = lon,                                         # x coordinate
+               y = lat,                                         # y coordinate
+               z = plot_var,                                    # z value to contour
+               zlim = c(min_plotting_range,                     # contour value range
+                        max_plotting_range),
+               color.palette = colorRampPalette(c("violet",     # color table
                                                   "blue",
                                                   "lightblue",
                                                   "yellow",
                                                   "orange",
-                                                  "red"
-                                                 )
+                                                  "red")
                                                ),
-               xlab = "Longitude", 
-               ylab = "Latitude",
-               plot.axes = {axis(1); axis(2);
-                            map(database = "world",
-                                xlim     = c(minlon, maxlon),
-                                ylim     = c(minlat, maxlat),
-                                add      = TRUE,
-                                col      = "black")
-                           },
-               main = paste("RCP 4.5 Air temp for ",
-                            time_months.45[target_t], "in deg C",
-                            sep="")
+               xlab = "Longitude",                              # x axis label
+               ylab = "Latitude",                               # y axis label
+               main = paste("RCP 4.5 ",                         # title string
+                            variable_name,
+                            " for ",
+                            time_months.45[target_t], 
+                            " in ",
+                            variable_units,
+                            sep=""),
+               plot.axes = {axis(1); axis(2);                   # mapping info begins
+                            map(database = "worldHires",  # map border dataset
+                                xlim     = c(minlon, maxlon),   # x axis range
+                                ylim     = c(minlat, maxlat),   # y axis range
+                                add      = TRUE,                # add to current plot
+                                col      = "black")             # map color
+                           }
               )
 
-plot_var =  t_2m.85[,,target_ens, target_t] - 273.17
+plot_var =  var.85[,,target_ens, target_t]
 
-filled.contour(x = lon, 
-               y = lat,
-               z = plot_var, 
-               color.palette = colorRampPalette(c("violet",
+# create contour map
+filled.contour(x = lon,                                         # x coordinate
+               y = lat,                                         # y coordinate
+               z = plot_var,                                    # z value to contour
+               zlim = c(min_plotting_range,                     # contour value range
+                        max_plotting_range),
+               color.palette = colorRampPalette(c("violet",     # color table
                                                   "blue",
                                                   "lightblue",
                                                   "yellow",
                                                   "orange",
-                                                  "red"
+                                                  "red")
+                                                ),
+               xlab = "Longitude",                              # x axis label
+               ylab = "Latitude",                               # y axis label
+               main = paste("RCP 8.5 ",                         # title string
+                            variable_name,
+                            " for ",
+                            time_months.45[target_t], 
+                            " in ",
+                            variable_units,
+                            sep=""),
+               plot.axes = {axis(1); axis(2);                   # mapping info begins
+                 map(database = "worldHires",                   # map border dataset
+                     xlim     = c(minlon, maxlon),              # x axis range
+                     ylim     = c(minlat, maxlat),              # y axis range
+                     add      = TRUE,                           # add to current plot
+                     col      = "black") }                       # map color
+                 plot.axes = {axis(1); axis(2);                   # mapping info begins
+                   map(database = "usa",                   # map border dataset
+                       xlim     = c(minlon, maxlon),              # x axis range
+                       ylim     = c(minlat, maxlat),              # y axis range
+                       add      = TRUE,                           # add to current plot
+                       col      = "black")                        # map color
+                 }
                )
-               ),
-               xlab = "Longitude", 
-               ylab = "Latitude",
-               plot.axes = {axis(1); axis(2);
-                 map(database = "world",
-                     xlim     = c(minlon, maxlon),
-                     ylim     = c(minlat, maxlat),
-                     add      = TRUE,
-                     col      = "black")
-               },
-               main = paste("RCP 8.5 Air temp for ",
-                            time_months.85[target_t], " in deg C",
-                            sep="")
-)
-
 
 
 
 # line plot
 
 
-plot(x = time_months.45,
-     y = t_2m.45[target_x,target_y,target_ens,],
-     main = paste("RCP 4.5 Air temp for Ens ",
+plot(x    = time_months.45,                        # x value
+     y    = var.45[target_x,target_y,target_ens,], # y value
+     type = "l",                                   # plot type
+     col  = "blue",                                # plot color
+     main = paste("RCP 4.5 ",                      # main title
+                  variable_name,
+                  " for Ens ",
                   target_ens,
                   " @ ",
                   target_lon, "E ",
                   target_lat, "N ",
-                  " in deg C",
+                  " in ",
+                  variable_units,
                   sep=""),
-     type = "l",
-     xlab = "Time",
-     ylab = "Temperature"
-    )
+     xlab = "Time",                                # x axis
+     ylab = variable_name,                         # y axis
+     ylim = c(min(c(var.45[target_x, target_y, target_ens, ],   # y axis range
+                    var.85[target_x, target_y, target_ens, ]),
+                  na.rm=TRUE),
+              max(c(var.45[target_x, target_y, target_ens, ],
+                    var.85[target_x, target_y, target_ens, ]),
+                  na.rm=TRUE)
+              )
+     )
 
 
-plot(x = time_months.85,
-     y = t_2m.85[target_x, target_y, target_ens,],
-     main = paste("RCP 4.5 Air temp for Ens ",
+plot(x    = time_months.85,                        # x value
+     y    = var.45[target_x,target_y,target_ens,], # y value
+     type = "l",                                   # plot type
+     col  = "red",                                 # plot color
+     main = paste("RCP 8.5 ",                      # main title
+                  variable_name,
+                  " for Ens ",
                   target_ens,
                   " @ ",
                   target_lon, "E ",
                   target_lat, "N ",
-                  " in deg C",
+                  " in ",
+                  variable_units,
                   sep=""),
-     type = "l",
-     xlab = "Time",
-     ylab = "Temperature"
-)
+     xlab = "Time",                                # x axis
+     ylab = variable_name,                         # y axis
+     ylim = c(min(c(var.45[target_x, target_y, target_ens, ],   # y axis range
+                    var.85[target_x, target_y, target_ens, ]),
+                  na.rm=TRUE),
+              max(c(var.45[target_x, target_y, target_ens, ],
+                    var.85[target_x, target_y, target_ens, ]),
+                  na.rm=TRUE)
+              )
+     )
 
